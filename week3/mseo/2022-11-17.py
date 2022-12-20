105537f9a345b50c51c69c87fe95ea05a7aed0a6
mseo39 <a01063294905@gmail.com>
2022-12-20 16:13:36 +0900

    week3

--- week3/mseo/2022-11-17.py
+++ week3/mseo/2022-11-17.py
@@ -1,25 +1,12 @@
 """
-어떤 정수들이 있습니다.
-이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와
-이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다
-실제 정수들의 합을 구하여 return하도록 solution 함수를
-완성해주세요
-
-참이면 양수고 거짓이면 음수이다
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어보세요
+단어의 길이가 짝수라면 가운데 두 글자를 반환하면 됩니다
 """
-def solution(absolutes, signs):
-    a=0
-    for i in range(len(absolutes)):
-        a=eval("a{}{}".format( "+" if signs[i]==True else "-",absolutes[i]))
-    return a
+def solution(s):
+    return (s[int(len(s)/2-1)]+s[int(len(s)/2)]) if len(s)%2==0 else s[int(len(s)/2)]
 
-print(solution([4,7,12],[True,False,True]))
 
 """
-print(exec("a=a{}{}".format(signs[i],absolutes[i]))
-이건 왜 안돌아갈까?
-
-def solution(absolutes, signs):
-    return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
-
+다른 사람들의 풀이
+return str[(len(str)-1)//2:len(str)//2+1]
 """
