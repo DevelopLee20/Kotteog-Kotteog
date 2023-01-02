0d3a57a33ad83e4241537fb25eb08dcfb27835f3
mseo39 <a01063294905@gmail.com>
2023-01-02 15:16:02 +0900

    23-01-02

--- week2/Mseo/2.py
+++ week2/Mseo/2.py
@@ -4,6 +4,9 @@
 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수를 완성하시오
 """
 def solution(x):
-    return 0
+    num=0 # 자릿수의 합을 저장할 변수
+    for i in str(x): #정수를 문자열로 만들고 for문으로 돌려서
+        num+=int(i)#전부 더헤줌
+    return True if x%num==0 else False #더한 값으로 x를 나눠주고 나머지가 0이면 True를 반환 아니면 False를 반환
 
-solution(10)
+print(solution(int(input())))
