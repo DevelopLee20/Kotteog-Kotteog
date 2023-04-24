74aaf9bd8490bafec99bdf610edc3767a3b9e50c
mseo39 <a01063294905@gmail.com>
2023-04-24 20:52:07 +0900

    create 스택

--- week5/Mseo/스택수열.py
+++ week5/Mseo/스택수열.py
@@ -1,6 +1,11 @@
-#스택
+#스택수열
 """
-정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오
+스택 (stack)은 기본적인 자료구조 중 하나로, 컴퓨터 프로그램을 작성할 때 자주 이용되는 개념이다. 
+스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 제일 나중에 들어간 자료가 제일 먼저 나오는 (LIFO, Last in First out) 특성을 가지고 있다.
+
+1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다. 
+이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 
+임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다. 이를 계산하는 프로그램을 작성하라.
 """
 import sys
 I=sys.stdin.readline
@@ -44,16 +49,3 @@ class stack:
 
 N=int(I())
 stack_=stack()
-for _ in range(N):
-    comment=I()
-    if "push" in comment:
-        temp=comment.split()
-        stack_.push(temp[1])
-    elif comment=="pop\n":
-        stack_.pop()
-    elif comment=="size\n":
-        print(stack_.size())
-    elif comment=="empty\n":
-        print(stack_.isEmpty())
-    elif comment=="top\n":
-        stack_.top()
